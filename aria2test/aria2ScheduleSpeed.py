c7820c1f0edeec11bdd7eb404b47018807daa58a
ntchris <ntchris@hotmail.com>
2017-11-22 23:39:54 -0500

    added time schedule

---
+++ aria2test/aria2ScheduleSpeed.py
@@ -0,0 +1,129 @@
+# to detect what is the service port for the aria2c rpc (well, the one I didn't started)
+
+import urllib2, json
+import threading
+import time
+from pprint import pprint
+
+myAria2cPort=9695
+secret='abcdmadpanz'
+param_secret="token:"+secret
+
+def listMethods():
+  global myAria2cPort
+  jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer', 'method':'system.listMethods'})
+  c = urllib2.urlopen("http://localhost:"+str(myAria2cPort)+"/jsonrpc", jsonreq)
+  pprint(json.loads(c.read()))
+
+
+found=False
+#{u'id': u'qwer',
+# u'jsonrpc': u'2.0',
+# u'result': [u'aria2.addUri',
+            #u'aria2.addTorrent',
+#...
+
+def listNotifications():
+  jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer', 'method':'system.listNotifications'})
+  c = urllib2.urlopen('http://localhost:6800/jsonrpc', jsonreq)
+  pprint(json.loads(c.read()))
+
+
+def listNotifications(port):
+  global found
+  print("testing "+str(port))
+  jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer', 'method':'system.listNotifications'})
+  urlbase="http://localhost"
+  urlservice="jsonrpc"
+  url=urlbase+":"+str(port)+"/"+urlservice
+  
+  try:
+    c = urllib2.urlopen(url, jsonreq)
+    pprint(json.loads(c.read()))
+    print("found! port is "+str(port)+"!!!")
+
+    found=True
+  except urllib2.URLError as error:
+    #print error
+    pass
+  
+  
+## found, 9695
+def findPort():
+  global found
+  for i in range (2, 65535):
+  #  listNotifications(i)
+     print("found is "+str(found))
+     if(found):
+        print ("found and break!")
+        break
+     threading.Thread(target=listNotifications, args=[i]).start()
+     time.sleep(0.01)
+  
+
+
+
+def getCurrentGlobalOptions():
+   global myAria2cPort
+   global param_secret
+  
+   jsonreq = json.dumps(  {'jsonrpc':'2.0', 'id':'qwer','method':'aria2.getGlobalOption','params':[param_secret]} )
+   print("!requesting "+jsonreq)
+   urlbase="http://localhost"
+   urlservice="jsonrpc"
+   url=urlbase+":"+str(myAria2cPort)+"/"+urlservice
+   
+   try:
+      resp = urllib2.urlopen(url, jsonreq)
+      goption=json.loads(resp.read())
+      print(goption)
+      result=goption['result']
+      print(result['max-overall-download-limit'])
+      
+   except urllib2.URLError as error:
+      print error
+      pass
+    
+   return
+  
+
+def setGlobalSpeedLimit(speedLimit):
+   global myAria2cPort
+   global param_secret
+   
+   options = {"max-overall-download-limit":speedLimit }
+   jsonreq = json.dumps(  {'jsonrpc':'2.0', 'id':'qwer','method':'aria2.changeGlobalOption','params':[param_secret , options ] } )
+   print("!requesting "+jsonreq)
+   urlbase="http://localhost"
+   urlservice="jsonrpc"
+   url=urlbase+":"+str(myAria2cPort)+"/"+urlservice
+   
+   try:
+      resp = urllib2.urlopen(url, jsonreq)
+      print(json.loads(resp.read()))
+   except urllib2.URLError as error:
+      print error
+      pass
+    
+   return
+  
+  
+
+  
+    
+def test():
+   listMethods()
+   getCurrentGlobalOptions()
+   setGlobalSpeedLimit("101")   
+   getCurrentGlobalOptions()
+ 
+      
+def main():
+   
+   test()
+
+
+main()
+
+
+  
---
+++ aria2test/aria2test.py
@@ -0,0 +1,68 @@
+# to detect what is the service port for the aria2c rpc (well, the one I didn't started)
+
+import urllib2, json
+import threading
+import time
+from pprint import pprint
+
+dupanAriaPort=9695
+
+def listMethods():
+  global dupanAriaPort
+  jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer', 'method':'system.listMethods'})
+  c = urllib2.urlopen("http://localhost:"+str(dupanAriaPort)+"/jsonrpc", jsonreq)
+  pprint(json.loads(c.read()))
+
+
+found=False
+#{u'id': u'qwer',
+# u'jsonrpc': u'2.0',
+# u'result': [u'aria2.addUri',
+            #u'aria2.addTorrent',
+#...
+
+def listNotifications():
+  jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer', 'method':'system.listNotifications'})
+  c = urllib2.urlopen('http://localhost:6800/jsonrpc', jsonreq)
+  pprint(json.loads(c.read()))
+
+
+def listNotifications(port):
+  global found
+  print("testing "+str(port))
+  jsonreq = json.dumps({'jsonrpc':'2.0', 'id':'qwer', 'method':'system.listNotifications'})
+  urlbase="http://localhost"
+  urlservice="jsonrpc"
+  url=urlbase+":"+str(port)+"/"+urlservice
+  
+  try:
+    c = urllib2.urlopen(url, jsonreq)
+    pprint(json.loads(c.read()))
+    print("found! port is "+str(port)+"!!!")
+
+    found=True
+  except urllib2.URLError as error:
+    #print error
+    pass
+  
+  
+## found, 9695
+def findPort():
+  global found
+  for i in range (2, 65535):
+  #  listNotifications(i)
+     print("found is "+str(found))
+     if(found):
+        print ("found and break!")
+        break
+     threading.Thread(target=listNotifications, args=[i]).start()
+     time.sleep(0.01)
+  
+  
+  
+@def main():
+   #listMethods()
+  #listNotifications(6800)
+  @pass
+  
+@main()
